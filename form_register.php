583333b4e40937755d8cc1f5d829219df19d3ddd
ThanasitAio <98828237+ThanasitAio@users.noreply.github.com>
2022-02-04 22:00:44 +0700

    Update form_register.php

--- form_register.php
+++ form_register.php
@@ -8,47 +8,49 @@
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $create_date = date("Y-m-d H:i:s");
-
-    $sql ="SELECT * FROM `user` WHERE username = :username";
-    $select_stmt = $db->query($sql);
-    $select_stmt->bindParam(":username",username);
+    $new_password = password_hash($password, PASSWORD_DEFAULT);
+    $G_user = 'user';
+    
+    $select_stmt = $db->prepare("SELECT * FROM user WHERE username = :username");
+    $select_stmt->bindParam(":username",$username);
     $select_stmt->execute();
     $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
-    $username_check = $row['username'];
+    $username_check = @$row['username'];
 
-     if($username_check == ''){
-            $G_user = 'user';
-            $new_password = password_hash($password, PASSWORD_DEFAULT);
-            $sql1 = "INSERT INTO user (username, email, password, first_name, last_name, create_date, group_user) VALUES (:username,:email,:password,:first_name,:last_name,:create_date,:group_user)";
-            $insert_stmt = $db->prepare($sql1);
-            $insert_stmt->bindParam(":username",$username);
-            $insert_stmt->bindParam(":email",$email);
-            $insert_stmt->bindParam(":password",$new_password);
-            $insert_stmt->bindParam(":first_name",$first_name);
-            $insert_stmt->bindParam(":last_name",$last_name);
-            $insert_stmt->bindParam(":group_user",$G_user);
-            $insert_stmt->bindParam(":create_date",$create_date, PDO::PARAM_STR);
-            $result = $insert_stmt->execute();
-         
-             $sql ="SELECT * FROM `user` WHERE username = :username";
-            $select_stmt = $db->query($sql);
-            $select_stmt->bindParam(":username",username);
-            $select_stmt->execute();
-            $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
-            $new_username = $row['username'];
-            if( $new_username == ''){
-               echo 'ERROR FALSE';
-               exit(); 
-            }else{
-                echo 'Register';
-                exit(); 
-            }
-        }else {
-             echo 'USERNAME FALSE';
-             exit();
-     }
+    if($username_check == ''){
+      $stmt = $db->prepare("INSERT INTO user (username, email, password, first_name, last_name, create_date, group_user)
+      VALUES (:username, :email, :password, :first_name, :last_name, :create_date, :group_user)");
+      $stmt->bindParam(":username",$username);
+      $stmt->bindParam(":email",$email);
+      $stmt->bindParam(":password",$new_password);
+      $stmt->bindParam(":first_name",$first_name);
+      $stmt->bindParam(":last_name",$last_name);
+      $stmt->bindParam(":create_date",$create_date);
+      $stmt->bindParam(":group_user",$G_user);
+      $result = $stmt->execute();
+      if($result){
+        $select_stmt1 = $db->prepare("SELECT * FROM user WHERE username = :username");
+        $select_stmt1->bindParam(":username",$username);
+        $select_stmt1->execute();
+        $row1 = $select_stmt1->fetch(PDO::FETCH_ASSOC);
+        $username_new = @$row1['username'];
+          if($username_new != ''){
+              echo 'Register';
+              exit();
+          }else{
+            echo 'ERROR FALSE';
+            exit();
+          }
 
+      }else{
+        echo 'ERROR FALSE';
+        exit();
+      }
 
-        
-   
+    }else {
+      echo 'USERNAME FALSE';
+      exit();
+    } 
+	
 ?>
+

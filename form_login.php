21114e91c9454db146c1baa1f45660a3665f2e84
ThanasitAio <98828237+ThanasitAio@users.noreply.github.com>
2022-02-04 22:43:29 +0700

    Update form_login.php

--- form_login.php
+++ form_login.php
@@ -1,45 +1,40 @@
 <?php
-    session_start();
-    require_once "db.php";
+  session_start();
+  require_once "db.php";
 
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $new_password = password_hash($password, PASSWORD_DEFAULT);
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $sql = "SELECT * FROM user WHERE username = :username AND password = :password";
-    $select_stmt = $db->query($sql);
-    $insert_stmt->bindParam(":username",$username);
-    $insert_stmt->bindParam(":password",$new_password);
-    $select_stmt->execute();
-    $row = $select_stmt->fetch(PDO::FETCH_ASSOC);
- 
-    if ($select_stmt->rowCount() > 0) {  
-                $_SESSION['username'] = $row['user_id'];
-              
-                if($row['group_user'] == 'admin'){
-                    echo 'LOGIN ADMIN';
-                    exit();
-                }else if($row['group_user'] == 'user'){
-                    echo 'LOGIN USER';
-                    exit();
-                }       
+
+  $stmt = $db->prepare("SELECT * FROM user WHERE username = :username");
+  $stmt->bindParam(":username",$username);
+  $stmt->execute();
+  $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if($stmt->rowCount() > 0){
+      if($username == @$row['username']){
+          $checked = password_verify($password, $hash);
+          if($checked){
+              if(@$row['group_user'] == 'admin'){
+                  echo 'LOGIN ADMIN';
+                  exit();
+              }else if(@$row['group_user'] == 'user'){
+                  echo 'LOGIN USER';
+                  exit();
+              }  
+          }else{
+              echo 'ERROR USERNAME';
+              exit();
+          }
+      }else{
+          echo 'ERROR USERNAME OR PASSWORD';
+          exit();
+      }
     }else{
         echo 'ERROR USERNAME OR PASSWORD';
         exit();
     }
-
-   
-
-
-
-    
-    
-
-
-    
-
-
-
-
+     
 ?>

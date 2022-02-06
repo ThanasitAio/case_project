609c067b839ff7ec72aacd3e65019049fef6e015
ThanasitAio <98828237+ThanasitAio@users.noreply.github.com>
2022-02-06 22:23:56 +0700

    Update Sidebar_profile_user.php

--- inc/Sidebar_profile_user.php
+++ inc/Sidebar_profile_user.php
@@ -6,11 +6,11 @@
         </div>
     </ion-header-bar>
     <ul>
-        <li><a href="profile_user.php" onclick="ACC()"><i class="fas fa-home"></i>บัญชีของฉัน</a></li>
+        <li><a href="Profile_user.php" onclick="ACC()"><i class="fas fa-home"></i>บัญชีของฉัน</a></li>
         <li><a href="#" onclick="BUY()"><i class="fas fa-user"></i>การซื้อของฉัน</a></li>
         <li><a href="#" onclick="SEND()"><i class="fas fa-address-card"></i>ข้อมูลที่อยู่จัดส่งสินค้า</a></li>
         <li><a href="#" onclick="LIKE()"><i class="fas fa-project-diagram"></i>รายการโปรดของคุณ</a></li>
-        <li><a href="edit_profile_user.php" onclick="DATE_ACC()"><i class="fas fa-blog"></i>ข้อมูลบัญชี</a></li>
+        <li><a href="Edit_profile_user.php" onclick="DATE_ACC()"><i class="fas fa-blog"></i>ข้อมูลบัญชี</a></li>
     </ul> 
 </div>
 

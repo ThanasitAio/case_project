4fdc673c442a7b96349d2b084ad7eb62f9509471
ThanasitAio <98828237+ThanasitAio@users.noreply.github.com>
2022-02-06 22:18:28 +0700

    Add files via upload

---
+++ inc/Sidebar_profile_user.php
@@ -0,0 +1,31 @@
+<div class="sidebar">
+    <ion-header-bar class="bar-profile">
+        <div class="profile">
+            <img class="profile-picture" src="../../img/rap.jpg" />
+            <h4 class="name">Thanasit Aiosakun</h4>
+        </div>
+    </ion-header-bar>
+    <ul>
+        <li><a href="profile_user.php" onclick="ACC()"><i class="fas fa-home"></i>บัญชีของฉัน</a></li>
+        <li><a href="#" onclick="BUY()"><i class="fas fa-user"></i>การซื้อของฉัน</a></li>
+        <li><a href="#" onclick="SEND()"><i class="fas fa-address-card"></i>ข้อมูลที่อยู่จัดส่งสินค้า</a></li>
+        <li><a href="#" onclick="LIKE()"><i class="fas fa-project-diagram"></i>รายการโปรดของคุณ</a></li>
+        <li><a href="edit_profile_user.php" onclick="DATE_ACC()"><i class="fas fa-blog"></i>ข้อมูลบัญชี</a></li>
+    </ul> 
+</div>
+
+    <script type="text/javascript">
+/*
+    function ACC(){
+        $.ajax({
+        url : "Acc_user.php",
+        type: "GET",
+        success:function(result){
+        $('#TableDetail').html(result);
+
+        }
+        });
+    }
+TableDetail();
+*/
+    </script>
---
+++ inc/menu.php
@@ -0,0 +1,14 @@
+<nav>
+        <div class="nav-bar">
+            <span class="logo"><a href="">CASER</a></span>
+        
+            <div class="menu">
+                <ul class="nav-links">
+                    <li><a href="">หน้าหลัก</a></li>
+                    <li><a href="">ข้อมูลผู้ใช้</a></li>
+                    <li><a href="">ประวัติขายสินค้า</a></li>
+                </ul>
+            </div>
+          
+         </div>
+    </nav>
